ba20e157d1968a80b3b2948f2819050e28373f1a
FrMnJ <manuelserratossec38@gmail.com>
2025-09-11 21:37:26 -0600

    configure neotree mappings

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,4 +7,9 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     lazy = false,
+    config = function()
+        vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = "Toggle Neo-tree" })
+        vim.keymap.set('n', '<leader>o', ':Neotree focus<CR>', { desc = "Focus Neo-tree" })
+        vim.keymap.set('n', '<leader>f', ':Neotree reveal<CR>', { desc = "Reveal current file in Neo-tree" })
+    end
 }
