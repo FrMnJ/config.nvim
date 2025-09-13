06da395bf505fc1cc72d474bc7057115990d93a9
FrMnJ <manuelserratossec38@gmail.com>
2025-09-12 18:22:22 -0600

    delete open terminal

--- lua/jose/mapping.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<leader>t', ':belowright split | terminal<CR>', { desc = "Open terminal in bottom split" })
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = "Exit terminal mode" })
-vim.keymap.set('n', '<leader>c', ':q<CR>', { desc = "Close current window" })
