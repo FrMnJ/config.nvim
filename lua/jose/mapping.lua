59b2c43a83d0cd924b3651f8fe449eedf47cc597
FrMnJ <manuelserratossec38@gmail.com>
2025-09-12 19:39:15 -0600

    add mapping for adding windows v and h and cw to close window

--- lua/jose/mapping.lua
+++ lua/jose/mapping.lua
@@ -10,3 +10,16 @@ vim.keymap.set("t", "<A-j>", [[<C-\><C-n><C-w>j]], { noremap = true, silent = tr
 vim.keymap.set("t", "<A-k>", [[<C-\><C-n><C-w>k]], { noremap = true, silent = true })
 vim.keymap.set("t", "<A-l>", [[<C-\><C-n><C-w>l]], { noremap = true, silent = true })
 
+-- Normal mode: split windows using | and -
+-- Vertical split with |
+vim.keymap.set("n", "|", ":vsplit<CR>", { noremap = true, silent = true })
+
+-- Horizontal split with -
+vim.keymap.set("n", "-", ":split<CR>", { noremap = true, silent = true })
+
+-- Optional: move to the new split immediately
+vim.keymap.set("n", "<Leader>|", ":vsplit<CR><C-w>l", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>-", ":split<CR><C-w>j", { noremap = true, silent = true })
+
+-- Close the current split with cw
+vim.keymap.set("n", "cw", "<C-w>c", { noremap = true, silent = true })
