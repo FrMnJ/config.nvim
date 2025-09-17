ebc78b11020a9052994d728e1f673d9f93962bc8
FrMnJ <jose.serratos1273@alumnos.udg.mx>
2025-09-17 14:23:17 -0600

    add galaxy line

---
+++ lua/plugins/galaxyline.lua
@@ -0,0 +1,7 @@
+return {
+    "NTBBloodbath/galaxyline.nvim",
+    config = function()
+        require("galaxyline.themes.eviline")
+    end,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+}
